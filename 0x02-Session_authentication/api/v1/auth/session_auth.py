30204908cc67ce567f26ed8a0e773f08c32a9299
alishakor <aliuokeowo@gmail.com>
2023-09-07 19:11:25 +0100

    creating a new session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,21 @@
 """Session auth class
 """
 from api.v1.auth.auth import Auth
+from typing import Dict
+import uuid
 
 
 class SessionAuth(Auth):
     """inherits from the parent class - Auth"""
 
-    pass
+    user_id_by_session_id: Dict = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a session ID"""
+        if user_id is None:
+            return None
+        if type(user_id) != str:
+            return None
+        session_id = str(uuid.uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
