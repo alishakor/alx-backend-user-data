4a31dae27bef6de0755803d6fb4deeac358e2a02
alishakor <aliuokeowo@gmail.com>
2023-09-08 03:12:52 +0100

    Modifying expiration date and adding sessions in databases

---
+++ 0x02-Session_authentication/models/user.session.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""module for user session
+"""
+from models.base import Base
+
+
+class UserSession(Base):
+    """inherits from parent class - Base"""
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """Initializes a User session instance.
+        """
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
