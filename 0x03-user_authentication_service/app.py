16f61e8ae1d40e0ce16e249a6527aa0318ff7300
alishakor <aliuokeowo@gmail.com>
2023-09-15 06:12:25 +0100

    modified app.py to register user in db

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,15 +2,34 @@
 """
 basic flask module
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, request
+from auth import Auth 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=["GET"], strict_slashes=False)
 def index():
-    """return index route"""
+    """GET /
+    Return:
+        - index route
+    """
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=["POST"], strict_slashes=False)
+def register_user():
+    """POST /users
+    Return: 
+        - Register user to database
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
