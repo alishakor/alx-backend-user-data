69b774f5bac4b6adc4d2823c928185ed447bc438
alishakor <aliuokeowo@gmail.com>
2023-09-16 18:56:21 +0100

    a function that route the Login in details with its sessions

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -33,5 +33,19 @@ def register_user():
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=["POST"], strict_slashes=False)
+def login():
+    """POST /sessions"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        user_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", user_id)
+        return response
+    else:
+        abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
