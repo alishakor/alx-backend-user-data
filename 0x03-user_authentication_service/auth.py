edf41966ca25d7d9712b52d0f4390bc257bef73c
alishakor <aliuokeowo@gmail.com>
2023-09-16 18:24:08 +0100

    fixing syntax error

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,7 +17,7 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
-def _generate_uuid(): -> str:
+def _generate_uuid() -> str:
     """generate a unique id"""
     return str(uuid4)
 
