dd83bab0e546799391b86d233ac57380e043b17d
alishakor <aliuokeowo@gmail.com>
2023-09-19 00:30:34 +0100

    Get User profile route, reset and update password using token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -46,6 +46,50 @@ def login():
     else:
         abort(401)
 
+    @app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+    def logout() -> str:
+        """DELETE /sessions"""
+        session_id = request.cookies.get("session_id")
+        user = AUTH.get_user_from_session_id(session_id)
+        if user is None:
+            abort(403)
+        AUTH.destroy_session(user.id)
+        return redirect("/")
+
+    @app.route("/profile", methods=["GET"], strict_slashes=False)
+    def profile() -> str:
+        """GET /profile"""
+        session_id = request.cookies.get("session_id")
+        user = AUTH.get_user_from_session_id(session_id)
+        if user is None:
+            abort(403)
+        return jsonify({"email": user.email})
+
+    @app.route('/reset_password', methods=['POST'], strict_slashes=False)
+    def get_reset_password_token():
+        """reset user password"""
+        email = request.form.get('email')
+        try:
+            token = AUTH.get_reset_password_token(email)
+            return jsonify(
+                {"email": f"{email}", "reset_token": f"{token}"}), 200
+        except ValueError:
+            abort(403)
+
+    @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+    def update_password():
+        """Updates a user password"""
+        email = request.form.get('email')
+        reset_token = request.form.get('reset_token')
+        new_password = request.form.get('new_password')
+        try:
+            if reset_token:
+                AUTH.update_password(reset_token, new_password)
+                return jsonify({"email": f"{email}",
+                                "message": "Password updated"}), 200
+        except ValueError:
+            abort(403)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -76,3 +76,25 @@ class Auth:
         if user_id is None:
             return None
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate a reset password token to a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+        except NoResultFound:
+            raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Updates a user's password using reset token"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            if user is None:
+                raise ValueError
+            new_hashed_pwd = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=new_hashed_pwd,
+                                 reset_token=None)
+        except Exception:
+            raise ValueError
